19e79fd5aaa8095ef8373982830706b29fb5e5e1
Mateusz Kiełbasa <sobriodev@gmail.com>
2020-10-05 17:10:03 +0200

    Add TEST_ASSERT_EQUAL_INT32 macro inside VT_PrintCommand() function

--- test/vt100_test_suite.c
+++ test/vt100_test_suite.c
@@ -23,33 +23,44 @@ void test_vt100_InvalidLutIndexes_VT_PrintCommand_ErrorReturned(void)
 
 void test_vt100_VT_PrintCommand_Works(void)
 {
-    /* It cannot be unit tested */
-
 #ifdef VT_TEST_VERBOSE
     printf("-------- VT100 TEST --------\n");
 
-    VT_PrintCommand(VT_ColorRed, VT_ColorDefault, VT_FormatNone);
+    TEST_ASSERT_EQUAL_INT32(
+                VT_StatusOk,
+                VT_PrintCommand(VT_ColorRed, VT_ColorDefault, VT_FormatNone));
     printf("The text should be red without any extras\n");
 
-    VT_PrintCommand(VT_ColorRed, VT_ColorDefault, VT_FormatBold);
+    TEST_ASSERT_EQUAL_INT32(
+                VT_StatusOk,
+                VT_PrintCommand(VT_ColorRed, VT_ColorDefault, VT_FormatBold));
     printf("The text should be bold red without any extras\n");
 
-    VT_PrintCommand(
-                VT_ColorBlue,
-                VT_ColorYellow,
-                VT_FormatBold | VT_FormatUnderlined | VT_FormatBlink);
+    TEST_ASSERT_EQUAL_INT32(
+                VT_StatusOk,
+                VT_PrintCommand(
+                    VT_ColorBlue,
+                    VT_ColorYellow,
+                    VT_FormatBold | VT_FormatUnderlined | VT_FormatBlink));
     printf("The text should bold blue with yellow foreground"
            " and underlined/blink extras\n");
 
-    VT_PrintCommand(
-                VT_ColorCyan,
-                VT_ColorMagenta,
-                VT_FormatBold | VT_FormatUnderlined
-                | VT_FormatBlink | VT_FormatInverted);
+    TEST_ASSERT_EQUAL_INT32(
+                VT_StatusOk,
+                VT_PrintCommand(
+                    VT_ColorCyan,
+                    VT_ColorMagenta,
+                    VT_FormatBold | VT_FormatUnderlined
+                    | VT_FormatBlink | VT_FormatInverted));
     printf("The text should bold magenta with cyan foreground"
            " and underlined/blink extras because invert flag is set\n");
 
-    VT_PrintCommand(VT_ColorGreen, VT_ColorDefault, VT_FormatUnderlined);
+    TEST_ASSERT_EQUAL_INT32(
+                VT_StatusOk,
+                VT_PrintCommand(
+                    VT_ColorGreen,
+                    VT_ColorDefault,
+                    VT_FormatUnderlined));
     printf("The text should be green and underlined\n");
     VT_ResetAllCommand();
 
@@ -57,6 +68,4 @@ void test_vt100_VT_PrintCommand_Works(void)
 
     printf("-------- VT100 TEST END --------\n");
 #endif
-
-    TEST_ASSERT_TRUE(true); /* Dummy test pass */
 }
