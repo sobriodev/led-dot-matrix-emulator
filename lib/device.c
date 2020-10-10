4939bb7c56df986675751a9b4d945fb33c6e9a25
Mateusz Kiełbasa <sobriodev@gmail.com>
2020-10-10 11:05:28 +0200

    Add device data types and API functions
    This commit is related to the #5 feature.
    Add DEV_Status and DEV_Color enums.
    Add DEV_Info struct type
    Create placeholders for DEV_CreateDevice and DEV_DestroyDevice API functions

--- lib/device.c
+++ lib/device.c
@@ -1,1 +1,14 @@
 #include "device.h"
+
+DEV_Status DEV_CreateDevice(DEV_Info* devInfo, DEV_Color color)
+{
+    (void)devInfo;
+    (void)color;
+    return DEV_StatusOK;
+}
+
+DEV_Status DEV_DestroyDevice(DEV_Info** devInfo)
+{
+    (void)(devInfo);
+    return DEV_StatusOK;
+}
--- lib/device.h
+++ lib/device.h
@@ -5,7 +5,37 @@
 extern "C" {
 #endif
 
+/* ------------------------------------------------------------------------- */
+/* ------------------------------ DATA TYPES ------------------------------- */
+/* ------------------------------------------------------------------------- */
 
+/* Device status codes */
+typedef enum
+{
+    DEV_StatusOK, /* OK */
+    DEV_StatusMemErr /* Memory allocation error */
+} DEV_Status;
+
+/* Possible LED colors */
+typedef enum
+{
+    DEV_ColorRed,
+    DEV_ColorGreen,
+    DEV_ColorBlue
+} DEV_Color;
+
+/* Device info */
+typedef struct
+{
+    /* TODO add contents */
+} DEV_Info;
+
+/* ------------------------------------------------------------------------- */
+/* ----------------------------- API FUNCTIONS ----------------------------- */
+/* ------------------------------------------------------------------------- */
+
+DEV_Status DEV_CreateDevice(DEV_Info* devInfo, DEV_Color color);
+DEV_Status DEV_DestroyDevice(DEV_Info** devInfo);
 
 #if defined(__cplusplus)
 }
