75e389737d54eb3849ed3c90765488060ace74d3
Mateusz Kiełbasa <sobriodev@gmail.com>
2020-10-11 15:07:17 +0200

    Add REG_Memory struct
    Related to #9
    Add register memory type
    Fix typo inside REG_Map

--- lib/register.h
+++ lib/register.h
@@ -1,6 +1,8 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
+#include "common.h"
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
@@ -23,10 +25,28 @@ typedef enum
     REG_MapDigit7 = 0x08,
     REG_MapDecodeMode = 0x09,
     REG_MapIntensity = 0x0A,
-    REG_MapsScanLimit = 0x0B,
+    REG_MapScanLimit = 0x0B,
     REG_MapShutdown = 0x0C,
     REG_MapDisplayTest = 0x0F
 } REG_Map;
+
+/* Register memory */
+typedef struct
+{
+    u8 digit0;
+    u8 digit1;
+    u8 digit2;
+    u8 digit3;
+    u8 digit4;
+    u8 digit5;
+    u8 digit6;
+    u8 digit7;
+    u8 decodeMode;
+    u8 intensity;
+    u8 scanLimit;
+    u8 shutdown;
+    u8 displayTest;
+} REG_Memory;
 
 #if defined(__cplusplus)
 }
